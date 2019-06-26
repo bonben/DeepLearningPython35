328d709b8bcbb246969ed2a3aa3a0c6982b5c5e6
Mathieu Léonardon <mathieu.leonardon@gmail.com>
2019-06-26 10:10:30 +0200

    Update theano test from http://deeplearning.net/software/theano/tutorial/using_gpu.html#testing-theano-with-gpu.

--- test.py
+++ test.py
@@ -17,11 +17,9 @@
 
 # ----------------------
 # - read the input data:
-'''
 import mnist_loader
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 training_data = list(training_data)
-'''
 # ---------------------
 # - network.py example:
 #import network
@@ -125,17 +123,16 @@ net.SGD(training_data, 30, 10, 0.1,
 
 """
 def testTheano():
-    from theano import function, config, shared, sandbox
-    import theano.tensor
+    from theano import function, config, shared, tensor
     import numpy
     import time
-    print("Testing Theano library...")
+
     vlen = 10 * 30 * 768  # 10 x #cores x # threads per core
     iters = 1000
 
     rng = numpy.random.RandomState(22)
     x = shared(numpy.asarray(rng.rand(vlen), config.floatX))
-    f = function([], theano.tensor.exp(x))
+    f = function([], tensor.exp(x))
     print(f.maker.fgraph.toposort())
     t0 = time.time()
     for i in range(iters):
@@ -143,12 +140,14 @@ def testTheano():
     t1 = time.time()
     print("Looping %d times took %f seconds" % (iters, t1 - t0))
     print("Result is %s" % (r,))
-    if numpy.any([isinstance(x.op, theano.tensor.Elemwise) for x in f.maker.fgraph.toposort()]):
+    if numpy.any([isinstance(x.op, tensor.Elemwise) and
+                  ('Gpu' not in type(x.op).__name__)
+                  for x in f.maker.fgraph.toposort()]):
         print('Used the cpu')
     else:
         print('Used the gpu')
 # Perform check:
-#testTheano()
+testTheano()
 
 
 # ----------------------
@@ -157,9 +156,11 @@ import network3
 from network3 import Network, ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer # softmax plus log-likelihood cost is more common in modern image classification networks.
 
 # read data:
+'''
 training_data, validation_data, test_data = network3.load_data_shared()
 # mini-batch size:
 mini_batch_size = 10
+'''
 
 # chapter 6 - shallow architecture using just a single hidden layer, containing 100 hidden neurons.
 '''
@@ -196,6 +197,7 @@ net.SGD(training_data, 60, mini_batch_size, 0.1, validation_data, test_data)
 
 # chapter 6 -  rectified linear units and some l2 regularization (lmbda=0.1) => even better accuracy
 from network3 import ReLU
+'''
 net = Network([
     ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
                   filter_shape=(20, 1, 5, 5),
@@ -208,3 +210,4 @@ net = Network([
     FullyConnectedLayer(n_in=40*4*4, n_out=100, activation_fn=ReLU),
     SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
 net.SGD(training_data, 60, mini_batch_size, 0.03, validation_data, test_data, lmbda=0.1)
+'''
